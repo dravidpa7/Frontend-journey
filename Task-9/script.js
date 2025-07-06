5b035d100f9aef72119333887c096a91b06c0afb
dravidpa7 <dravidpa113@gmail.com>
2025-07-06 23:33:46 +0530

    Task 9 : basic animation with Dom Manupulation

--- Task-9/script.js
+++ Task-9/script.js
@@ -1,12 +1,18 @@
 function on(){
-    document.querySelector(".char").style.animationPlayState = 'running';
-    document.querySelector('button:last-child').style.backgroundColor='rgb(255, 73, 73)';
-    document.querySelector(".spinner").style.animationPlayState = 'running';    
+    document.querySelector(".char").style.animationPlayState = 'paused';
+    document.querySelector(".spinner").style.animationPlayState = 'paused';  
+    document.querySelector('.start-btn').style.display='block';
+    document.querySelector('.pause-btn').style.display='none';  
+
+    for(let i=0;i<10;i++){
+        console.log(i);
+        document.getElementById("count").innerHTML=i;
+    }
 }
 
 function off(){
-    document.querySelector(".char").style.animationPlayState = 'paused';
-    document.querySelector('button:last-child').style.backgroundColor='rgb(201, 8, 8)';
-    document.querySelector(".spinner").style.animationPlayState = 'paused';    
-
+    document.querySelector(".char").style.animationPlayState = 'running';
+    document.querySelector(".spinner").style.animationPlayState = 'running'; 
+    document.querySelector('.start-btn').style.display='none'; 
+    document.querySelector('.pause-btn').style.display='block';      
 }
